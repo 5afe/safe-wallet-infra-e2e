fd50ddcdd4421c3852eba1eb78f04da956498764
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2024-04-03 16:25:44 +0200

    Add a second Safe

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { configuration } from '@/config/configuration';
 import { EOARepository } from '@/domain/eoa/eoa-repository';
+import { SafeType } from '@/domain/safes/entities/safe-type';
 import { SafesRepository } from '@/domain/safes/safes-repository';
 import 'dotenv/config';
 
 const { privateKeys } = configuration;
 
+async function init(safesRepository: SafesRepository): Promise<void> {
+  await safesRepository.deploySafe(SafeType.PRIMARY);
+  await safesRepository.deploySafe(SafeType.SECONDARY);
+}
+
 async function main() {
   const safesRepository = new SafesRepository(privateKeys[0]);
-  await safesRepository.init();
+  await init(safesRepository);
 
   const eoaRepository = new EOARepository();
   await eoaRepository.equilibrateBalances();
