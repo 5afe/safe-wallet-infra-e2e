b07d4517ebab5c1a5fc9343193eaabb22affe68d
Hector Gómez Varela <hector@safe.global>
2024-07-05 12:46:08 +0200

    Add CGW accounts data settings tests

--- src/__tests__/cgw/accounts.spec.ts
+++ src/__tests__/cgw/accounts.spec.ts
@@ -36,26 +36,130 @@ describe('CGW Auth tests', () => {
   let created: CGWAccount;
 
   describe('auth endpoints', () => {
-    it('should create an account, get and delete', async () => {
+    it('should create an account, get it, and delete it', async () => {
+      const createAccountDto = { address: walletAddresses[0] };
+      const { address } = createAccountDto;
+
       try {
-        const createAccountDto = {
-          address: walletAddresses[0],
-        };
-
         created = await cgw.createAccount(accessToken, createAccountDto);
-
         expect(created).toBeDefined();
-        expect(created.address).toBe(createAccountDto.address);
-
+        expect(created.address).toBe(address);
         const account = await cgw.getAccount(accessToken, created.address);
         expect(account.address).toBe(created.address);
         expect(account.accountId).toBe(created.accountId);
       } finally {
-        await cgw.deleteAccount(accessToken, created.address);
-        await expect(
-          cgw.getAccount(accessToken, created.address),
-        ).rejects.toThrow();
+        await cgw.deleteAccount(accessToken, address);
       }
+
+      await expect(cgw.getAccount(accessToken, address)).rejects.toThrow(
+        'Request failed with status code 404',
+      );
+    });
+
+    it('should get the available data types', async () => {
+      const dataTypes = await cgw.getDataTypes();
+      expect(dataTypes).toStrictEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            dataTypeId: expect.any(String),
+            name: 'CounterfactualSafes',
+            description: expect.any(String),
+            isActive: expect.any(Boolean),
+          }),
+          expect.objectContaining({
+            dataTypeId: expect.any(String),
+            name: 'AddressBook',
+            description: expect.any(String),
+            isActive: expect.any(Boolean),
+          }),
+        ]),
+      );
+    });
+
+    it('should create an account, set its data settings twice, and delete the account', async () => {
+      const createAccountDto = { address: walletAddresses[0] };
+      const { address } = createAccountDto;
+
+      try {
+        created = await cgw.createAccount(accessToken, createAccountDto);
+        expect(created).toBeDefined();
+        expect(created.address).toBe(address);
+        const account = await cgw.getAccount(accessToken, created.address);
+        expect(account.address).toBe(created.address);
+        expect(account.accountId).toBe(created.accountId);
+        const dataTypes = await cgw.getDataTypes();
+        const upsertAccountDataSettingsDto = {
+          accountDataSettings: dataTypes.map((dt) => ({
+            dataTypeName: dt.name,
+            enabled: true,
+          })),
+        };
+
+        const actual = await cgw.upsertAccountDataSettings(
+          accessToken,
+          address,
+          upsertAccountDataSettingsDto,
+        );
+
+        expect(actual).toStrictEqual(
+          expect.arrayContaining([
+            {
+              dataTypeName: dataTypes[0].name,
+              dataTypeDescription: dataTypes[0].description,
+              enabled: true,
+            },
+            {
+              dataTypeName: dataTypes[1].name,
+              dataTypeDescription: dataTypes[1].description,
+              enabled: true,
+            },
+            {
+              dataTypeName: dataTypes[2].name,
+              dataTypeDescription: dataTypes[2].description,
+              enabled: true,
+            },
+          ]),
+        );
+
+        const anotherUpsertAccountDataSettingsDto = {
+          accountDataSettings: dataTypes.map((dt) => ({
+            dataTypeName: dt.name,
+            enabled: false,
+          })),
+        };
+
+        const updated = await cgw.upsertAccountDataSettings(
+          accessToken,
+          address,
+          anotherUpsertAccountDataSettingsDto,
+        );
+
+        expect(updated).toStrictEqual(
+          expect.arrayContaining([
+            {
+              dataTypeName: dataTypes[0].name,
+              dataTypeDescription: dataTypes[0].description,
+              enabled: false,
+            },
+            {
+              dataTypeName: dataTypes[1].name,
+              dataTypeDescription: dataTypes[1].description,
+              enabled: false,
+            },
+            {
+              dataTypeName: dataTypes[2].name,
+              dataTypeDescription: dataTypes[2].description,
+              enabled: false,
+            },
+          ]),
+        );
+      } finally {
+        await cgw.deleteAccount(accessToken, address);
+      }
+
+      await expect(cgw.getAccount(accessToken, address)).rejects.toThrow(
+        'Request failed with status code 404',
+      );
     });
   });
 });
