7d571c4b7a4ff187f4e6b426ec153c7e3cb6c9db
Hector Gómez Varela <hector@safe.global>
2024-07-11 12:57:25 +0200

    Restore CGW baseUri

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,8 +6,7 @@ export const configuration = {
     name: 'sepolia',
   },
   clientGateway: {
-    baseUri: 'http://localhost:4000',
-    // baseUri: 'https://safe-client.staging.5afe.dev',
+    baseUri: 'https://safe-client.staging.5afe.dev',
   },
   privateKeys: [
     process.env.PRIVATE_KEY as `0x${string}`,
